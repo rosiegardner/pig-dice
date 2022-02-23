fd547c3782eee779a2373b0bb835f09d9d7563f3
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-22 20:50:42 -0800

    WIP: Hold button function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 // Business Logic
 
-const player01 = new Player(0, 0, 0, true);
-const player02 = new Player(0, 0, 0, false);
-
-function Player(overallScore, currentScore, rollScore, turn) {
+function Player(overallScore, currentScore, rollScore) {
   this.overallScore = overallScore; // Total amount of points
   this.currentScore = currentScore; // Current Score before rolling 1 or holding
   this.rollScore = rollScore; // Current value of die
-  this.isTurn = turn; // dictates which player is currently rolling
+  this.isTurn = true; // dictates which player is currently rolling
 }
 
 Player.prototype.randomRoll = function() {
@@ -18,32 +15,23 @@ Player.prototype.randomRoll = function() {
 Player.prototype.updateCurrentScore = function() {
   if (this.rollScore === 1) {
     this.currentScore = 0;
-    this.isTurn = false; 
+    this.switchTurn();
   } else {
     this.currentScore += this.rollScore;
   }
   return this.currentScore
 }
 
-Player.prototype.disablePlayer = function() {
-  if (this.isTurn === false) {
-
+ Player.prototype.switchTurn = function() {
+    this.isTurn = !this.isTurn 
+    return this.isTurn;
   }
-}
-
-// if its 1 or you click hold, switch player
-// If we do an if/else statement saying if click.HOLD button in HTML, is that technically mixing business & user interface logic. 
-// If we click hold, add the value of the currentScore to the overallScore
 
 Player.prototype.hold = function() {
   this.overallScore += this.currentScore;
   this.currentScore = 0;
   return this.overallScore;
 }
-
-// we need to reset current score afterwards. 
-// we are going to dim out when turn = false; 
-//
 
 //Player.prototype.hold = function() {
 //  this.overallScore += this.currentScore;
@@ -62,46 +50,87 @@ Player.prototype.hold = function() {
 
 // User interface
 
+
+
 $(document).ready(function() {
+  const player01 = new Player(0, 0, 0);
+  const player02 = new Player(0, 0, 0);
+  player02.isTurn = false; 
+
+  // click roll 
   $("button#p1-button-roll").click(function(event) {
     event.preventDefault();
     let currentRoll = player01.randomRoll();
     let currentScore = player01.updateCurrentScore();
     $("#player1-rollDie").text(currentRoll);
     $("#current-score1").text(currentScore);
-    // if player clicks hold, add currentScore to overallScore
-  });
-});
 
-$(document).ready(function() {
-  $("button#p2-button-roll").click(function(event) {
+  // click hold
+    $("button#p1-button-hold").click(function(event)  {
     event.preventDefault();
-    let currentRoll2 = player02.randomRoll();
-    let currentScore2 = player02.updateCurrentScore();
-    $("#player2-rollDie").text(currentRoll2);
-    $("#current-score2").text(currentScore2);
-  });
-});
-
-$(document).ready(function() {
-  $("button#p1-button-hold").click(function(event)  {
-    event.preventDefault();
+    player01.switchTurn();
     let overallScore = player01.hold();
     let currentScore = 0;
     $("#overall-score1").text(overallScore);
     $("#current-score1").text(currentScore);
+    $("#p1-button-hold").hide();
+    $("#p1-button-roll").hide();
+    $("#p2-button-hold").show();
+    $("#p2-button-roll").show();
+    });
   });
 });
+
+// After hold button on line 68, 
+
+
+// $(document).ready(function() {
+//   $("button#p1-button-hold").click(function(event)  {
+//     event.preventDefault();
+//     let overallScore = player01.hold();
+//     let currentScore = 0;
+//     $("#overall-score1").text(overallScore);
+//     $("#current-score1").text(currentScore);
+//     $("#p1-button-hold").hide();
+//     $("#p1-button-roll").hide();
+//     $("#p2-button-hold").show();
+//     $("#p2-button-roll").show();    
+//   });
+// });
+
+// $(document).ready(function() {
+//   $("button#p2-button-roll").click(function(event) {
+//     event.preventDefault();
+//     let currentRoll2 = player02.randomRoll();
+//     let currentScore2 = player02.updateCurrentScore();
+//     $("#player2-rollDie").text(currentRoll2);
+//     $("#current-score2").text(currentScore2);
+//   });
+// });
 
 $(document).ready(function() {
   $("button#p2-button-hold").click(function(event)  {
     event.preventDefault();
     let overallScore2 = player02.hold();
     let currentScore2 = 0;
+    $("#p2-button-roll").hide();
+    $("#p2-button-hold").hide();
+    $("#p1-button-roll").show();
+    $("#p1-button-hold").show();   
     $("#overall-score2").text(overallScore2);
     $("#current-score2").text(currentScore2);  
   });
 });
+
+
+// if (this.isTurn === false) {
+//   $("button#p2-button-roll").hide();
+//   $("button#p2-button-hold").hide();
+// }
+//  else {
+//   $("button#p2-button-roll").show();
+//   $("button#p2-button-hold").show();
+// }
 
 // Start with disabled flag on Player two because isTurn === false; 
 
